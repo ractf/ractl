dc43e05debeb449ac1dc5798b4290d9a0ab955d4
Daniel Milnes <daniel.milnes@lmax.com>
2021-05-09 16:42:23 +0100

    Get package version from Cargo

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod install;
 use clap::{App, SubCommand};
 
 fn main() {
-    let matches = App::new("ractfctl")
-        .version("0.1")
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
         .author("RACTF Admins <admins@ractf.co.uk>")
         .about("RACTF Admin CLI")
         .subcommand(install::subcommand())
